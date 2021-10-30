bc24e922ed68e8da73d128f003fec4b857ace472
lezcano65 <lezcano65@hotmail.com>
2021-10-30 12:05:30 -0300

    agregando el main para probar el codigo

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         //probando
+        Profesor profe = new Profesor("facundo","uferer",12345,10,2);
+        Materia materia1 = new Materia("Programacion 2",profe);
         Facultad facultad = new Facultad("UTN-FRR");
         facultad.agregarCarrera(new Carrera("ISI"));
         facultad.agregarCarrera(new Carrera("TUP"));
         facultad.agregarCarrera(new Carrera("IQ"));
-        facultad.listarContenido();
+        System.out.println(facultad.listarContenido());
         facultad.eliminarCarrera("IQ");
-        facultad.listarContenido();
-
-
+        System.out.println(facultad.listarContenido());
 
     }
 }
